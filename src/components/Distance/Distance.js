2c6ce9430d1a5259c9ebe7a8f763e86b64477370
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2022-06-03 12:58:10 +0530

    final commit

--- src/components/Distance/Distance.js
+++ src/components/Distance/Distance.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import "./Distance.css";
 
 function Distance(props) {
+  const [distance, setDistance] = useState("");
+
+  useEffect(() => {
+    if (props.showDetails) {
+      var service = new window.google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [props.origin],
+          destinations: [props.destination],
+          travelMode: "DRIVING",
+        },
+        callback
+      );
+
+      function callback(response, status) {
+        if (response.rows[0].elements[0].distance)
+          setDistance(response.rows[0].elements[0].distance.text);
+        else setDistance("No Direct Route Found !!");
+      }
+    }
+  }, [props.origin, props.destination, props.showDetails]);
+
   return (
-    <div>
+    <div className='distance-details'>
       <div className='distance-container'>
         <p>Distance</p>
-        {!props.showDetails ? "" : <h3>{}</h3>}
+        {!props.showDetails ? "" : <h3>{distance}</h3>}
       </div>
       <div className='distance-text'>
         {!props.showDetails ? (
@@ -13,7 +36,7 @@ function Distance(props) {
         ) : (
           <p>
             The distance between <strong>{props.origin}</strong> and{" "}
-            <strong>{props.destination}</strong> is <strong>{}kms</strong>
+            <strong>{props.destination}</strong> is <strong>{distance}</strong>
           </p>
         )}
       </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoLocationSharp } from "react-icons/io5";
 import Distance from "../Distance/Distance";
 import "./Main.css";
 
@@ -9,14 +10,16 @@ function Main() {
 
   function onChangeOrigin(e) {
     setOrigin(e.target.value);
+    if (showDetails) setShowDetails(false);
   }
 
   function onChangeDestination(e) {
     setDestination(e.target.value);
+    if (showDetails) setShowDetails(false);
   }
 
   function handleCalculate(e) {
-      setShowDetails(true);
+    setShowDetails(true);
   }
 
   return (
@@ -27,14 +30,16 @@ function Main() {
       <div className='container'>
         <div className='form'>
           <div className='form-container'>
-            <div>
+            <div className='input-container'>
               <label>Origin</label>
+              <IoLocationSharp className='icon1'></IoLocationSharp>
               <input
                 type='text'
                 onChange={onChangeOrigin}
                 value={origin}
               ></input>
               <label>Destination</label>
+              <IoLocationSharp className='icon2'></IoLocationSharp>
               <input
                 type='text'
                 onChange={onChangeDestination}
@@ -43,9 +48,33 @@ function Main() {
             </div>
             <button onClick={handleCalculate}>Calculate</button>
           </div>
-          <Distance origin={origin} destination={destination} showDetails={showDetails}></Distance>
+          <Distance
+            origin={origin}
+            destination={destination}
+            showDetails={showDetails}
+          ></Distance>
         </div>
-        <div className='map'>Hello</div>
+        <div className='map'>
+          {!showDetails ? (
+            <iframe
+              title='Static Map'
+              width='600'
+              height='450'
+              style={{ border: 0 }}
+              allowFullScreen
+              src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJkbeSa_BfYzARphNChaFPjNc&key=${process.env.REACT_APP_GOOGLE_MAP_IFRAME_KEY}`}
+            ></iframe>
+          ) : (
+            <iframe
+              title='Direction Map'
+              width='600'
+              height='450'
+              style={{ border: 0 }}
+              allowFullScreen
+              src={`https://www.google.com/maps/embed/v1/directions?origin=${origin}&destination=${destination}&key=${process.env.REACT_APP_GOOGLE_MAP_IFRAME_KEY}`}
+            ></iframe>
+          )}
+        </div>
       </div>
     </main>
   );
